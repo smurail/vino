08d6122f740aa4bf746ea71689748764bf7eff21
smurail <simon.murail@iscpif.fr>
2019-10-08 18:23:47 +0200

    refactor(loaders): clean pspmodifiedloader

--- vino/loaders/pspmodifiedloader.py
+++ vino/loaders/pspmodifiedloader.py
@@ -6,51 +6,61 @@ from vino.loaders import FileFormatLoader
 
 class PspModifiedLoader(FileFormatLoader):
     '''
-    Reader for the modified output format of the software of Patrick Saint-Pierre.
-    By "modified", it means that the raw output file has been modified for give easy access to metadata at the begin of the file.
+    Reader for the modified output format of the software of Patrick
+    Saint-Pierre.  By "modified", it means that the raw output file has been
+    modified for give easy access to metadata at the begin of the file.
     '''
 
     @overrides
     def readFile(self, f):
         '''
-        Returns an object of class BarGridKernel loaded from an output file from the software of Patrick Saint-Pierre.
+        Returns an object of class BarGridKernel loaded from an output file
+        from the software of Patrick Saint-Pierre.
         '''
+
         bgk = None
+
         origin = list(map(float, re.findall('-?\d+\.?\d*', f.readline())))
         dimension = len(origin)
-        if dimension == 0 :
-            raise FileFormatException("Dimensions must be > 0")
+
+        if dimension == 0:
+            raise FileFormatException("Couldn't find more than 0 dimension")
+
         opposite = list(map(float, re.findall('-?\d+\.?\d*', f.readline())))
-        intervalNumber = list(map(int, re.findall('[0-9]+', f.readline())))
-        if dimension !=len(opposite) or dimension!=len(intervalNumber):
+        intervalNumber = list(map(int, re.findall('\d+', f.readline())))
+
+        if dimension != len(opposite) or dimension != len(intervalNumber):
             raise FileFormatException("Dimensions of metadata mismatch")
-        pointSize = list(map(int, re.findall('[0-9]+', f.readline())))
-        intervalNumber = [e//pointSize[0] for e in intervalNumber]
-        # reading columns headers and deducing permutation of variables
+
+        # Read point size
         line = f.readline()
-        columnNumbertoIgnore = len(re.findall('empty', line))
+        pointSize = next(map(int, re.findall('\d+', line)), None)
+        intervalNumber = [e // pointSize for e in intervalNumber]
+
+        # Read columns headers
+        line = f.readline()
+        ignoreColumns = len(re.findall('empty', line))
+
+        # Make permutation matrix
         permutVector = list(map(int, re.findall('[0-9]+', line)))
         permutation = np.zeros((dimension, dimension), int)
         for i in range(dimension):
-            permutation[i][permutVector[i]-1]=1
-        # Ok, creating the container object
-        bgk = BarGridKernel(origin, opposite, intervalNumber,permutation)
-        # ignoring lines until 'Initxx'
-        stop=False
-        while not stop:
+            permutation[i][permutVector[i]-1] = 1
+
+        # Create container object
+        bgk = BarGridKernel(origin, opposite, intervalNumber, permutation)
+
+        # Ignore lines until 'Initxx'
+        while True:
             line = f.readline()
             if 'Initxx' in line:
-                stop = True
-        # reading bars
-        stop = False
-        while not stop:
-            # using a while loop, because the for loop seems buggy with django InMemoryUploadedFile reading
-            line = f.readline()
-            if not line:
-                stop = True
-            else:
-                coords = list(map(int, re.findall('[0-9]+', line)))
-                coords = [e // pointSize[0] for e in coords]
-                bgk.addBar(coords[columnNumbertoIgnore:-2], coords[-2], coords[-1])
-                # TODO what is done with modelMetadata and nbDim
+                break
+
+        # Read bars
+        for line in f:
+            coords = list(map(int, re.findall('\d+', line)))
+            coords = [e // pointSize for e in coords]
+            bgk.addBar(coords[ignoreColumns:-2], coords[-2], coords[-1])
+            # TODO what is done with modelMetadata and nbDim
+
         return bgk
