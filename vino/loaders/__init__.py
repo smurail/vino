5cefbdcef14a6c0b9e942bf28cc9fd3d79b72fdb
smurail <simon.murail@iscpif.fr>
2019-10-15 14:30:20 +0200

    refactor(loaders): move Loader to __init__.py

--- vino/loaders/__init__.py
+++ vino/loaders/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from .base import Loader, FileFormatException, FileFormatLoader
 from .psploader import PspLoader
 from .pspmodifiedloader import PspModifiedLoader
@@ -6,5 +8,28 @@ from .hdf5loader import Hdf5Loader
 
 __all__ = [
     'Loader', 'FileFormatException', 'FileFormatLoader', 'PspLoader',
-    'PspModifiedLoader', 'ViabilitreeLoader', 'Hdf5Loader',
+    'PspModifiedLoader', 'ViabilitreeLoader', 'Hdf5Loader', 'Loader',
 ]
+
+
+class Loader(object):
+    def __init__(self):
+        self.loaders = []
+
+    def loadersdoc(self):
+        for loader in self.loaders:
+            print("{0}: {1}".format(type(loader).__name__, str(loader.__doc__)))
+
+    def load(self, filename):
+        '''
+        Load a file by trying all file loaders, and return an object of type Kernel, precisely on of its subtypes.
+        Returns None if no suitable loader have succeed to load the file.
+        '''
+
+        for loader in self.loaders:
+            try:
+                return loader.read(filename)
+            except Exception as e:
+                logging.getLogger(__name__).info("Loading of %s fails with the %s loader: %s",filename, loader, e)
+
+        return None
--- vino/loaders/base.py
+++ vino/loaders/base.py
@@ -1,28 +1,4 @@
-import logging
 from abc import ABC
-
-
-class Loader(object):
-    def __init__(self):
-        self.loaders = []
-
-    def loadersdoc(self):
-        for loader in self.loaders:
-            print("{0}: {1}".format(type(loader).__name__, str(loader.__doc__)))
-
-    def load(self, filename):
-        '''
-        Load a file by trying all file loaders, and return an object of type Kernel, precisely on of its subtypes.
-        Returns None if no suitable loader have succeed to load the file.
-        '''
-
-        for loader in self.loaders:
-            try:
-                return loader.read(filename)
-            except Exception as e:
-                logging.getLogger(__name__).info("Loading of %s fails with the %s loader: %s",filename, loader, e)
-
-        return None
 
 
 class FileFormatException(Exception):
