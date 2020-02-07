eeccb38ebfa38cf4c2c3fa1e9cfc6452b8bc565c
smurail <simon.murail@iscpif.fr>
2020-02-07 17:15:10 +0100

    chore(models): add ParameterSet.to_dict method

--- vino/sharekernel/models.py
+++ vino/sharekernel/models.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict, defaultdict
+from itertools import chain
 
 from django.db import models
 
@@ -157,6 +158,17 @@ class ParameterSet(Entity):
     dynamics = models.CharField(max_length=200, blank=True)
     constraints = models.CharField(max_length=200, blank=True)
     target = models.CharField(max_length=200, blank=True)
+
+    def to_dict(self):
+        types = [Symbol.DYNAMICS, Symbol.CONSTRAINT, Symbol.TARGET]
+        # List all names from symbols within `types`
+        symbols = self.vp.symbols.filter(type__in=types).order_by('type').all()
+        sorted_symbols = sorted(symbols, key=lambda s: types.index(s.type))
+        names = (symbol.name for symbol in sorted_symbols)
+        # List all values from those types
+        values_set = (self.dynamics, self.constraints, self.target)
+        values = chain.from_iterable((x.split(',') for x in values_set))
+        return OrderedDict(zip(names, values))
 
     def __str__(self):
         fields = filter(None, (self.dynamics, self.constraints, self.target))
