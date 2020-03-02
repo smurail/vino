377a8133caa1c70fadd9d0efe5a000f5434e9bc8
smurail <simon.murail@iscpif.fr>
2020-03-02 15:59:18 +0100

    chore(utils): filter out composed functions

--- vino/core/utils.py
+++ vino/core/utils.py
@@ -4,6 +4,8 @@ from functools import reduce
 
 
 def compose(*functions):
+    functions = [f for f in functions if callable(f)]
+
     def inner(arg):
         return reduce(lambda arg, func: func(arg), functions, arg)
     return inner
