1342b073bb69c41199fa33f0f674d2501f17c8d6
smurail <simon.murail@iscpif.fr>
2020-03-07 18:07:10 +0100

    chore(admin): customize Kernel admin

--- vino/sharekernel/admin.py
+++ vino/sharekernel/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.utils.html import escape
+
 from .importable import ImportableMixin
 from .models import Symbol, ViabilityProblem, ParameterSet, Software, Kernel
 
@@ -32,9 +36,21 @@ class ViabilityProblemAdmin(admin.ModelAdmin):
 
 @admin.register(Kernel)
 class KernelAdmin(ImportableMixin, admin.ModelAdmin):
-    list_display = ('title', 'owner', 'state', 'date_created', 'date_updated')
-    readonly_fields = ('date_created', 'date_updated')
+    list_display = (
+        'vp', 'title', 'format', 'software', 'owner', 'state',
+        'date_created', 'date_updated')
+    list_display_links = ('title',)
+    readonly_fields = ('date_created', 'date_updated', 'vp')
+    ordering = ('params__vp__title', 'title')
     fields = (
         'owner', 'state', 'title', 'date_created', 'date_updated',
-        'params', 'format', 'software', 'datafile',
+        'vp', 'params', 'format', 'software', 'datafile',
         'description', 'publication', 'author', 'email', 'url', 'image')
+
+    def vp(self, obj):
+        vp = obj.params.vp
+        url = reverse("admin:sharekernel_viabilityproblem_change", args=(vp.pk,))
+        title = obj.params.vp.title
+        return mark_safe(f'<a href="{escape(url)}">{escape(title)}</a>')
+    vp.short_description = 'Viability problem'  # type: ignore
+    vp.admin_order_field = 'params__vp__title'  # type: ignore
