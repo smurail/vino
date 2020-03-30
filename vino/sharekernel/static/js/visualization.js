d0cdc21ef0d0b2ff86ba8c0d1e1351f67218c04b
smurail <simon.murail@iscpif.fr>
2020-03-30 17:48:26 +0200

    refactor(visualization): refactor javascript Visualization class

--- vino/sharekernel/static/js/visualization.js
+++ vino/sharekernel/static/js/visualization.js
@@ -1,14 +1,24 @@
+"use strict";
+
 class Visualization {
     constructor(element) {
         this.element = element instanceof HTMLElement ?
                        element :
                        document.querySelector(element);
-        this.update();
+        this.loader = this.element.querySelector('.loader');
+        setTimeout(this.load.bind(this), 1);
+    }
+
+    dispatchEvent(name, details) {
+        this.element.dispatchEvent(new CustomEvent(name, details));
+    }
+
+    addEventListener(name, callback) {
+        this.element.addEventListener(name, callback);
     }
 
     loading(isLoading) {
-        var loader = this.element.querySelector('.loader');
-        loader.classList.toggle('visible', isLoading);
+        this.loader.classList.toggle('visible', isLoading);
     }
 
     plot(data) {
@@ -23,23 +33,24 @@ class Visualization {
                 orientation: 'h'
             };
 
+        this.dispatchEvent('plotstart', {data: data});
+
         for (const p in data)
             trace[p] = data[p];
 
         Plotly.react(view, [trace], layout);
+
+        this.dispatchEvent('plotend');
     }
 
-    load(onLoad) {
-        var url = this.element.querySelector('input[name=url]');
+    load() {
+        this.dispatchEvent('load');
         this.loading(true);
+        var url = this.element.querySelector('input[name=url]');
         fetch(url.value)
             .then(r => r.json())
-            .then(data => onLoad(data))
+            .then(data => this.plot(data))
             .catch(error => console.log(error))
             .finally(() => this.loading(false));
-    }
-
-    update() {
-        this.load(this.plot.bind(this));
     }
 }
