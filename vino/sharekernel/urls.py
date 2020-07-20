b16e111e9a7c6a295496b37263cdf680307e68e2
smurail <simon.murail@iscpif.fr>
2020-07-20 14:21:36 +0200

    feat(sharekernel): add About page

--- vino/sharekernel/urls.py
+++ vino/sharekernel/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from .views import HomeView, VisualizationDemoView, KernelData
 
@@ -7,6 +8,7 @@ from .views import HomeView, VisualizationDemoView, KernelData
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('visualization-demo', VisualizationDemoView.as_view(), name='visualization_demo'),
+    path('about', TemplateView.as_view(template_name='sharekernel/about.html'), name='about'),
     path('kernel/<int:pk>/data/', KernelData.as_view(), name='kernel_data'),
     path('kernel/<int:pk>/data/<int:ppa>/', KernelData.as_view()),
 ]
