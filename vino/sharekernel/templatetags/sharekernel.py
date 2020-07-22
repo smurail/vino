01e4405654b359178ee30da16b76cb4265441542
smurail <simon.murail@iscpif.fr>
2020-07-22 22:57:30 +0200

    feat(sharekernel): add kernel_url template tag

--- vino/sharekernel/templatetags/sharekernel.py
+++ vino/sharekernel/templatetags/sharekernel.py
@@ -1,6 +1,12 @@
 import re
 
 from django import template
+
+# For kernel_url tag
+from django.urls import reverse
+from django.utils.html import conditional_escape
+
+# For pluck filter
 from django.template import VariableDoesNotExist
 from django.template.defaultfilters import _property_resolver
 
@@ -22,6 +28,43 @@ def visualize(context, kernels=None):
         'id': state['id'],
         'kernels': kernels,
     }
+
+
+class KernelURLNode(template.Node):
+    VIEW_NAME = 'viabilityproblem'
+
+    def __init__(self, kernel):
+        self.arg = kernel
+
+    def render(self, context):
+        try:
+            current_app = context.request.current_app
+        except AttributeError:
+            try:
+                current_app = context.request.resolver_match.namespace
+            except:
+                current_app = None
+
+        kernel = self.arg.resolve(context)
+        args = [kernel.vp.pk]
+        url = reverse(self.VIEW_NAME, args=args, current_app=current_app)
+        url += f'#kernel/{kernel.pk}'
+
+        if context.autoescape:
+            url = conditional_escape(url)
+
+        return url
+
+
+@register.tag
+def kernel_url(parser, token):
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise TemplateSyntaxError("'%s' takes at least one argument, a Kernel instance." % bits[0])
+
+    kernel = parser.compile_filter(bits[1])
+
+    return KernelURLNode(kernel)
 
 
 @register.filter
