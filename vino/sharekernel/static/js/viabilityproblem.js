a5a3f837eac5c14e43c7a9c3b671c1e24b8401ea
smurail <simon.murail@iscpif.fr>
2020-07-22 22:26:29 +0200

    refactor(sharekernel): clean code in viabilityproblem.js

--- vino/sharekernel/static/js/viabilityproblem.js
+++ vino/sharekernel/static/js/viabilityproblem.js
@@ -1,7 +1,18 @@
+function selectKernel(id) {
+    // Check the checkbox and triggers change event to visualize it
+    $('#select-kernel-' + id)
+        .prop('checked', true)
+        .trigger('change');
+}
+
 $(function() {
     var kernels = $('#kernels tr[data-kernel-id]'),
         checkboxes = $('#kernels input[type=checkbox]'),
         currentKernel = $('.vz-container input[name=kernel]');
+
+    // If there is only one kernel, disable the checkbox
+    if (kernels.length == 1)
+        checkboxes.prop('disabled', true);
 
     // Attach change event to each kernel checkbox
     checkboxes.on('change', function() {
@@ -11,36 +22,17 @@ $(function() {
         // Update visualization
         currentKernel.val(row.data('kernel-id'));
 
-        // Trigger change event with DOM style because its visualize way
-        currentKernel[0].dispatchEvent(new Event('change'));
-
         // Colorize selected row
         row.toggleClass('table-primary', cb.prop('checked'));
 
         // Unselect all rows but selected row
         checkboxes.not(this).prop('checked', false)
             .closest('tr').removeClass('table-primary');
+
+        // Trigger change event with DOM style because its visualize way
+        currentKernel[0].dispatchEvent(new Event('change'));
     });
 
     // Select and show first kernel
-    kernels.each(function() {
-        var row = $(this),
-            kernelId = row.data('kernel-id'),
-            disabled = false;
-
-        if (kernelId) {
-            // If there is only one kernel, disable the checkbox
-            if (kernels.length == 1)
-                disabled = true;
-
-            // Check the checkbox and triggers change event to visualize it
-            $('#select-kernel-' + kernelId)
-                .prop('checked', true)
-                .prop('disabled', disabled)
-                .trigger('change');
-
-            // That's all Folks
-            return false;
-        }
-    });
+    selectKernel(kernels.first().data('kernel-id'));
 });
