1e48b05b467da5315ed8d6caf74a0e260142b58d
smurail <smurail@tutanota.com>
2021-01-11 11:02:53 +0100

    refactor(sharekernel): clean file storage utils

--- vino/sharekernel/utils.py
+++ vino/sharekernel/utils.py
@@ -1,35 +1,38 @@
 import datetime
 
 from pathlib import Path
+from os import PathLike
+from typing import Union, IO, AnyStr, Iterable
 
 from django.conf import settings
 from django.core.files import File
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import Storage, default_storage
 
 
-def generate_media_path(path):
+AnyPath = Union[str, PathLike]
+
+
+def interpolate_path(path: AnyPath):
+    if isinstance(path, PathLike):
+        path = path.as_posix()
     interpolated_path = datetime.datetime.now().strftime(path)
     return Path(settings.MEDIA_ROOT) / interpolated_path
 
 
-def as_django_file(f):
+def as_django_file(f: IO[AnyStr]):
     return f if isinstance(f, File) else File(f)
 
 
-def store_files(path, *files):
-    fs = FileSystemStorage(path)
-    return [
-        Path(fs.path(fs.save(fs.generate_filename(Path(f.name).name), f)))
-        for f in map(as_django_file, files)
-    ]
+def store_files(path: AnyPath, *files: IO[AnyStr], storage: Storage = default_storage):
+    if not isinstance(path, PathLike):
+        path = Path(path)
+    return [store_one_file(path / f.name, f, storage) for f in files]
 
 
-def store_one_file(filepath, content):
-    filepath = Path(filepath)
-    content = content if isinstance(content, File) else File(content)
-    fs = FileSystemStorage(filepath.parent)
-    return Path(fs.path(fs.save(fs.generate_filename(filepath.name), content)))
+def store_one_file(filepath: AnyPath, content: IO[AnyStr], storage: Storage = default_storage):
+    path = interpolate_path(filepath).as_posix()
+    return Path(storage.save(path, as_django_file(content)))
 
 
-def sorted_by_size(files):
+def sorted_by_size(files: Iterable[AnyPath]):
     return sorted(files, key=lambda f: Path(f).stat().st_size)
