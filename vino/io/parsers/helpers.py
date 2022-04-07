127832ed4077938f83b2a2fc409116bbd535e684
smurail <smurail@tutanota.com>
2022-04-07 16:48:15 +0200

    refactor(io): RichCSVParser supersedes ViabilitreeParser

--- vino/io/parsers/helpers.py
+++ vino/io/parsers/helpers.py
@@ -8,7 +8,6 @@ from ...core.numo import Numo
 from .exceptions import WrongFormatError
 from .metadataparser import MetadataParser
 from .pspparser import PSPParser
-from .viabilitreeparser import ViabilitreeParser
 from .richcsvparser import RichCSVParser
 
 
@@ -30,11 +29,5 @@ def sourcefile_parse(stream: TextIO) -> Union[Metadata, np.ndarray, Numo]:
     except WrongFormatError:
         stream.seek(0)
 
-    # 3. Is it a Viabilitree file?
-    try:
-        return ViabilitreeParser().parse(stream)
-    except WrongFormatError:
-        stream.seek(0)
-
-    # 4. Otherwise, we expect vino datafile
+    # 3. Otherwise, we expect a RichCSV file
     return RichCSVParser().parse(stream)
