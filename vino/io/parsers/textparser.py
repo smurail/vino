6dc8bc92640e904293abceb01cc53e78bc13c9b5
smurail <smurail@tutanota.com>
2022-04-07 10:51:08 +0200

    fix(io): better error message while decoding text

--- vino/io/parsers/textparser.py
+++ vino/io/parsers/textparser.py
@@ -7,5 +7,5 @@ class TextParserMixin:
     @staticmethod
     def handle_unicode_decode_error(stream: TextIO, e: UnicodeDecodeError, prefix: str = "") -> NoReturn:
         name = getattr(stream, 'name', '<unknown>')
-        msg = f'Input stream {name!r} seems to be binary and cannot be opened as text'
+        msg = f'Input stream {name!r} cannot be opened as utf-8 text and might be binary'
         raise WrongFormatError(prefix + msg) from e
