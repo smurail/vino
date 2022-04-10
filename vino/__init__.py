535f405f782bcc8826f1c3abef90ca3419481d6f
smurail <smurail@tutanota.com>
2022-04-10 20:43:46 +0200

    feat: add Vino subclasses to public api

--- vino/__init__.py
+++ vino/__init__.py
@@ -4,6 +4,10 @@ from vino.core import (
     Metadata,
     Numo,
     Vino,
+    RegularGrid,
+    BarGrid,
+    KdTree,
+    Polygon,
 )
 from vino.io import (
     load,
@@ -15,6 +19,6 @@ from vino.io import (
 
 
 __all__ = [
-    'Metadata', 'Numo', 'Vino',
+    'Metadata', 'Numo', 'Vino', 'RegularGrid', 'BarGrid', 'KdTree', 'Polygon',
     'load', 'save_csv', 'load_npy', 'load_npz', 'save_npz'
 ]
