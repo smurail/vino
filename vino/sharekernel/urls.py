72641c633388ae27103b22c8151cc310f0405ba3
smurail <smurail@tutanota.com>
2022-05-27 14:27:31 +0200

    refactor(sharekernel): add comments

--- vino/sharekernel/urls.py
+++ vino/sharekernel/urls.py
@@ -16,10 +16,13 @@ urlpatterns = [
     path('viabilityproblem/<int:pk>/', ViabilityProblemView.as_view(), name='viabilityproblem'),
     path('visualization-demo', VisualizationDemoView.as_view(), name='visualization_demo'),
     path('about', TemplateView.as_view(template_name='sharekernel/about.html'), name='about'),
+
+    # -- Old web API
     path('kernel/<int:pk>/data/', KernelData.as_view(), name='kernel_data'),
     path('kernel/<int:pk>/data/<int:ppa>/', KernelData.as_view()),
     path('kernel/<int:pk>/data/<int:ppa>/distance/', KernelData.as_view(), {'distance': True}),
 
+    # -- New web API
     # Original vino
     path('api/vino/<int:pk>/', VinoData.as_view(), name='vino_data'),
     path('api/vino/<int:pk>/info/', VinoData.as_view(info_only=True), name='vino_data'),
