189e754eba347aa31bbcae24c67ea66520d687c3
smurail <smurail@tutanota.com>
2022-06-06 17:52:18 +0200

    chore(sharekernel): add media_relative_path utility function

--- vino/sharekernel/utils.py
+++ vino/sharekernel/utils.py
@@ -6,6 +6,8 @@ import hashlib
 from pathlib import Path
 from typing import Union, Iterable
 
+from django.core.files.storage import default_storage
+
 from ..typing import AnyPath
 
 
@@ -14,6 +16,10 @@ StringPath = Union[str, os.PathLike]
 
 def sorted_by_size(files: Iterable[StringPath]) -> list[StringPath]:
     return sorted(files, key=lambda f: Path(f).stat().st_size)
+
+
+def media_relative_path(path: Path) -> str:
+    return path.relative_to(default_storage.location).as_posix()
 
 
 def hash_file(path: AnyPath, algorithm: str = 'sha1', chunk_size: int = 1024*1024) -> str:
